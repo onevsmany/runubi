449691a3c6666cd322b7ae26094f9d87ba090f80
Kyl67899 <35744262+Kyl67899@users.noreply.github.com>
2020-08-07 13:43:13 -0400

    search bar sample html, css, and js, need json database with the candidates to fetch the data

---
+++ search.js
@@ -0,0 +1,31 @@
+const charactersList = document.getElementById('charactersList');
+let hpCharacters = [];
+
+const loadCharacters = async () => {
+    try {
+
+        // need json script for the candidates profile cards. Something like this: https://hp-api.herokuapp.com/api/characters
+        const res = await fetch('');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
